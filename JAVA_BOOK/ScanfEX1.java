c34930c38609f22be4ab9a90a76a293ef2cca57d
yujinjo0111 <78085881+yujinjo0111@users.noreply.github.com>
2021-03-09 17:38:59 +0900

    Create ScanfEX1.java

---
+++ JAVA_BOOK/ScanfEX1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ScanfEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int num=scanner.nextInt();
+		int num2 = scanner.nextInt();
+
+		System.out.println(num);
+		System.out.println(num2);
+		}
+
+}
