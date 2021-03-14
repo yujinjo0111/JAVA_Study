c8d6dc26a1b642604edd113eda594ecf15246dbb
yujinjo0111 <78085881+yujinjo0111@users.noreply.github.com>
2021-03-14 23:05:47 +0900

    Create EX2_11.java

---
+++ JAVA_BOOK/EX2_11.java
@@ -0,0 +1,18 @@
+
+public class EX2_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short sMin =-32768;
+		short sMax=32767;
+		char cMin=0;
+		char cMax=65535;
+		System.out.println("sMin ="+sMin);
+		System.out.println("sMin-1 ="+(short)(sMin-1));
+		System.out.println("sMax ="+sMax);
+		
+		System.out.println("cMin ="+(int)cMin);
+		System.out.println("cMax ="+(int)cMax);
+	}
+
+}
